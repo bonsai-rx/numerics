2481e6e17e84fbb2a373ea529009f25a36b17ea7
glopes <glopes@localhost>
2018-07-01 18:49:00 +0100

    Refactored CreateRandom into the root numerics namespace.

--- Bonsai.Numerics/CreateRandom.cs
+++ Bonsai.Numerics/CreateRandom.cs
@@ -5,7 +5,7 @@ using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bonsai.Numerics.Distributions
+namespace Bonsai.Numerics
 {
     public class CreateRandom : Source<Random>
     {
