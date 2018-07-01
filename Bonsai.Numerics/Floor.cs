683197c8aef32557419164a41e8721c32ae0fc9a
glopes <glopesdev@users.noreply.github.com>
2018-07-01 20:40:04 +0100

    Added missing support for common mathematical functions.

---
+++ Bonsai.Numerics/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    public class Floor : Transform<double, double>
+    {
+        public override IObservable<double> Process(IObservable<double> source)
+        {
+            return source.Select(x => Math.Floor(x));
+        }
+
+        public IObservable<decimal> Process(IObservable<decimal> source)
+        {
+            return source.Select(x => Math.Floor(x));
+        }
+    }
+}
