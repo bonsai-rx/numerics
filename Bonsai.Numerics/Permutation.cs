fe00419ffd7b536042d08e5c7d93ee662fa5120f
glopes <glopesdev@users.noreply.github.com>
2018-07-01 18:51:24 +0100

    Added support for selecting random permutations of a data sequence.

---
+++ Bonsai.Numerics/Permutation.cs
@@ -0,0 +1,35 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class Permutation
+    {
+        public IObservable<TElement> Process<TElement>(IObservable<TElement> source)
+        {
+            return source
+                .ToArray()
+                .Do(elements => Combinatorics.SelectPermutationInplace(elements))
+                .SelectMany(elements => elements);
+        }
+
+        public IObservable<TElement> Process<TElement>(IObservable<TElement> source, IObservable<Random> random)
+        {
+            return source
+                .ToArray()
+                .Zip(random.FirstAsync(), (elements, randomSource) =>
+                {
+                    Combinatorics.SelectPermutationInplace(elements, randomSource);
+                    return elements;
+                })
+                .SelectMany(elements => elements);
+        }
+    }
+}
