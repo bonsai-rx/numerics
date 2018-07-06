0d1191ff7a5b2e1cb99f4a13dc719eff7b083288
glopes <glopes@localhost>
2018-07-06 02:24:16 +0100

    Added SampleMany operator.

---
+++ Bonsai.Numerics/Distributions/SampleMany.cs
@@ -0,0 +1,34 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics.Distributions
+{
+    [Combinator]
+    [Description("Draws multiple random samples from the input distribution.")]
+    public class SampleMany
+    {
+        public SampleMany()
+        {
+            Count = 1;
+        }
+
+        [Description("The number of random samples to draw.")]
+        public int Count { get; set; }
+
+        public IObservable<int> Process(IObservable<IDiscreteDistribution> source)
+        {
+            return source.SelectMany(distribution => distribution.Samples().Take(Count));
+        }
+
+        public IObservable<double> Process(IObservable<IContinuousDistribution> source)
+        {
+            return source.SelectMany(distribution => distribution.Samples().Take(Count));
+        }
+    }
+}
