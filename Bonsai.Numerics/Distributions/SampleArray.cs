ccc1f62ef173c093eb82deb809611e6b246d5469
glopes <glopes@localhost>
2018-11-20 16:20:25 +0000

    Added SampleArray operator.

---
+++ Bonsai.Numerics/Distributions/SampleArray.cs
@@ -0,0 +1,44 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics.Distributions
+{
+    [Combinator]
+    [Description("Draws a specified number of random samples from the input distribution into an array.")]
+    public class SampleArray
+    {
+        public SampleArray()
+        {
+            Count = 1;
+        }
+
+        [Description("The number of random samples in each output array.")]
+        public int Count { get; set; }
+
+        public IObservable<int[]> Process(IObservable<IDiscreteDistribution> source)
+        {
+            return source.Select(distribution =>
+            {
+                var result = new int[Count];
+                distribution.Samples(result);
+                return result;
+            });
+        }
+
+        public IObservable<double[]> Process(IObservable<IContinuousDistribution> source)
+        {
+            return source.Select(distribution =>
+            {
+                var result = new double[Count];
+                distribution.Samples(result);
+                return result;
+            });
+        }
+    }
+}
