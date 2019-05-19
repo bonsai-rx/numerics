5494549e05ec41eee83417f00ae44ab9e8ccf869
glopes <glopesdev@users.noreply.github.com>
2019-05-19 01:33:20 +0100

    Added angle conversion operators.

---
+++ Bonsai.Numerics/DegreeToRadian.cs
@@ -0,0 +1,20 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    [Description("Converts a degree (360-periodic) angle to a radian (2*Pi-periodic) angle.")]
+    public class DegreeToRadian : Transform<double, double>
+    {
+        public override IObservable<double> Process(IObservable<double> source)
+        {
+            return source.Select(Trig.DegreeToRadian);
+        }
+    }
+}
---
+++ Bonsai.Numerics/RadianToDegree.cs
@@ -0,0 +1,20 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    [Description("Converts a radian (2*Pi-periodic) angle to a degree (360-periodic) angle.")]
+    public class RadianToDegree : Transform<double, double>
+    {
+        public override IObservable<double> Process(IObservable<double> source)
+        {
+            return source.Select(Trig.RadianToDegree);
+        }
+    }
+}
