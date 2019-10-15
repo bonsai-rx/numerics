897ff1ebacb8e0fabb5bb55bc7b78b6d2808a6d1
glopes <glopes@localhost>
2019-10-15 11:54:53 +0100

    Added support for generating combinations and variations from array sequences.

---
+++ Bonsai.Numerics/CombinatoricsHelper.cs
@@ -0,0 +1,80 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    static class CombinatoricsHelper
+    {
+        private static TElement[] SelectVariation<TElement>(TElement[] elements, int[] indices)
+        {
+            var variation = new TElement[indices.Length];
+            for (int i = 0; i < variation.Length; i++)
+            {
+                variation[i] = elements[indices[i]];
+            }
+
+            return variation;
+        }
+
+        internal static TElement[] SelectVariation<TElement>(TElement[] elements, int count)
+        {
+            return SelectVariation(elements, count, null);
+        }
+
+        internal static TElement[] SelectVariation<TElement>(TElement[] elements, int count, Random randomSource)
+        {
+            return SelectVariation(elements, Combinatorics.GenerateVariation(elements.Length, count, randomSource));
+        }
+
+        internal static TElement[] SelectVariationWithRepetition<TElement>(TElement[] elements, int count)
+        {
+            return SelectVariationWithRepetition(elements, count, null);
+        }
+
+        internal static TElement[] SelectVariationWithRepetition<TElement>(TElement[] elements, int count, Random randomSource)
+        {
+            return SelectVariation(elements, Combinatorics.GenerateVariationWithRepetition(elements.Length, count, randomSource));
+        }
+
+        internal static TElement[] SelectCombination<TElement>(TElement[] elements, int count)
+        {
+            return SelectCombination(elements, count, null);
+        }
+
+        internal static TElement[] SelectCombination<TElement>(TElement[] elements, int count, Random randomSource)
+        {
+            var mask = Combinatorics.GenerateCombination(elements.Length, count, randomSource);
+            var combination = new TElement[count];
+            for (int i = 0, k = 0; i < mask.Length; i++)
+            {
+                if (mask[i]) combination[k++] = elements[i];
+            }
+
+            return combination;
+        }
+
+        internal static TElement[] SelectCombinationWithRepetition<TElement>(TElement[] elements, int count)
+        {
+            return SelectCombinationWithRepetition(elements, count, null);
+        }
+
+        internal static TElement[] SelectCombinationWithRepetition<TElement>(TElement[] elements, int count, Random randomSource)
+        {
+            var selected = Combinatorics.GenerateCombinationWithRepetition(elements.Length, count, randomSource);
+            var combination = new TElement[count];
+            for (int i = 0, k = 0; i < selected.Length; i++)
+            {
+                for (int j = 0; j < selected[i]; j++)
+                {
+                    combination[k++] = elements[i];
+                }
+            }
+
+            return combination;
+        }
+    }
+}
---
+++ Bonsai.Numerics/SelectVariation.cs
@@ -0,0 +1,42 @@
+﻿using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.Numerics
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Generates random variations, without repetition, by randomly selecting k elements from each array in the input sequence.")]
+    public class SelectVariation
+    {
+        [Description("The number of elements to choose from each array. Each element is chosen at most once.")]
+        public int Count { get; set; }
+
+        public IObservable<TElement[]> Process<TElement>(IObservable<TElement[]> source)
+        {
+            return source.Select(elements => CombinatoricsHelper.SelectVariation(elements, Count));
+        }
+
+        public IObservable<TElement[]> Process<TElement>(IObservable<TElement[]> source, IObservable<Random> random)
+        {
+            return random.FirstAsync().SelectMany(randomSource =>
+                source.Select(elements => CombinatoricsHelper.SelectVariation(elements, Count, randomSource)));
+        }
+
+        public IObservable<IEnumerable<TElement>> Process<TElement>(IObservable<IEnumerable<TElement>> source)
+        {
+            return source.Select(elements => Combinatorics.SelectVariation(elements, Count));
+        }
+
+        public IObservable<IEnumerable<TElement>> Process<TElement>(IObservable<IEnumerable<TElement>> source, IObservable<Random> random)
+        {
+            return random.FirstAsync().SelectMany(randomSource =>
+                source.Select(elements => Combinatorics.SelectVariation(elements, Count, randomSource)));
+        }
+    }
+}
