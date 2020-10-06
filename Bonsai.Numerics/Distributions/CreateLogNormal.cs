af926a295a04d7f2f0bfa192ec93161cb999ccb3
glopesdev <glopesdev@users.noreply.github.com>
2020-10-06 16:45:59 +0100

    Add support for log-normal distribution

---
+++ Bonsai.Numerics/Distributions/CreateLogNormal.cs
@@ -0,0 +1,33 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Numerics.Distributions
+{
+    [Description("Creates a continuous univariate log-normal distribution.")]
+    public class CreateLogNormal : Source<LogNormal>
+    {
+        public CreateLogNormal()
+        {
+            StdDev = 1;
+        }
+
+        [Description("The log-scale (μ) of the logarithm of the distribution.")]
+        public double Mean { get; set; }
+
+        [Description("The shape (σ) of the logarithm of the distribution.")]
+        public double StdDev { get; set; }
+
+        public override IObservable<LogNormal> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new LogNormal(Mean, StdDev)));
+        }
+
+        public IObservable<LogNormal> Generate(IObservable<Random> source)
+        {
+            return source.Select(random => new LogNormal(Mean, StdDev, random));
+        }
+    }
+}
