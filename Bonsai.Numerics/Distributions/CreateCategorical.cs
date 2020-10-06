c85c6806377ff1ec0a148ec8d6521d77a526a414
glopesdev <glopesdev@users.noreply.github.com>
2020-10-06 16:45:22 +0100

    Add support for categorical distribution

---
+++ Bonsai.Numerics/Distributions/CreateCategorical.cs
@@ -0,0 +1,26 @@
+﻿using MathNet.Numerics.Distributions;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Numerics.Distributions
+{
+    [Description("Creates a discrete univariate categorical distribution.")]
+    public class CreateCategorical : Source<Categorical>
+    {
+        [TypeConverter(typeof(UnidimensionalArrayConverter))]
+        [Description("An array of non-negative ratios representing the probability mass for each category. Normalization is not required.")]
+        public double[] ProbabilityMass { get; set; }
+
+        public override IObservable<Categorical> Generate()
+        {
+            return Observable.Defer(() => Observable.Return(new Categorical(ProbabilityMass)));
+        }
+
+        public IObservable<Categorical> Generate(IObservable<Random> source)
+        {
+            return source.Select(random => new Categorical(ProbabilityMass, random));
+        }
+    }
+}
