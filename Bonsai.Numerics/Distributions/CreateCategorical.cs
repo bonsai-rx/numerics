a60b429239a3dd0bcff49b76587b87366f501e9f
glopesdev <glopesdev@users.noreply.github.com>
2021-06-12 16:09:03 +0100

    Add default mass for categorical distribution

--- Bonsai.Numerics/Distributions/CreateCategorical.cs
+++ Bonsai.Numerics/Distributions/CreateCategorical.cs
@@ -9,18 +9,20 @@ namespace Bonsai.Numerics.Distributions
     [Description("Creates a discrete univariate categorical distribution.")]
     public class CreateCategorical : Source<Categorical>
     {
+        static readonly double[] DefaultProbability = new[] { 1.0 };
+
         [TypeConverter(typeof(UnidimensionalArrayConverter))]
         [Description("An array of non-negative ratios representing the probability mass for each category. Normalization is not required.")]
         public double[] ProbabilityMass { get; set; }
 
         public override IObservable<Categorical> Generate()
         {
-            return Observable.Defer(() => Observable.Return(new Categorical(ProbabilityMass)));
+            return Observable.Defer(() => Observable.Return(new Categorical(ProbabilityMass ?? DefaultProbability)));
         }
 
         public IObservable<Categorical> Generate(IObservable<Random> source)
         {
-            return source.Select(random => new Categorical(ProbabilityMass, random));
+            return source.Select(random => new Categorical(ProbabilityMass ?? DefaultProbability, random));
         }
     }
 }
