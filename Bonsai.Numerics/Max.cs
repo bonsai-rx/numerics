0cac34469d901a2d4d5e7af328be3cce69ee81a9
glopesdev <glopesdev@users.noreply.github.com>
2022-03-15 14:34:48 +0000

    Generalize min and max for any comparable type

--- Bonsai.Numerics/Max.cs
+++ Bonsai.Numerics/Max.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reactive.Linq;
 
 namespace Bonsai.Numerics
 {
-    [Description("Calculates the larger of two numbers.")]
+    [Description("Calculates the largest of a set of values.")]
     public class Max : Transform<Tuple<double, double>, double>
     {
         public override IObservable<double> Process(IObservable<Tuple<double, double>> source)
@@ -61,6 +62,144 @@ namespace Bonsai.Numerics
         public IObservable<byte> Process(IObservable<Tuple<byte, byte>> source)
         {
             return source.Select(input => Math.Max(input.Item1, input.Item2));
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input => comparer.Compare(input.Item1, input.Item2) > 0
+                ? input.Item1
+                : input.Item2);
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input =>
+            {
+                var value = input.Item1;
+                if (comparer.Compare(input.Item2, value) > 0) value = input.Item2;
+                if (comparer.Compare(input.Item3, value) > 0) value = input.Item3;
+                return value;
+            });
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource, TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input =>
+            {
+                var value = input.Item1;
+                if (comparer.Compare(input.Item2, value) > 0) value = input.Item2;
+                if (comparer.Compare(input.Item3, value) > 0) value = input.Item3;
+                if (comparer.Compare(input.Item4, value) > 0) value = input.Item4;
+                return value;
+            });
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource, TSource, TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input =>
+            {
+                var value = input.Item1;
+                if (comparer.Compare(input.Item2, value) > 0) value = input.Item2;
+                if (comparer.Compare(input.Item3, value) > 0) value = input.Item3;
+                if (comparer.Compare(input.Item4, value) > 0) value = input.Item4;
+                if (comparer.Compare(input.Item5, value) > 0) value = input.Item5;
+                return value;
+            });
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource, TSource, TSource, TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input =>
+            {
+                var value = input.Item1;
+                if (comparer.Compare(input.Item2, value) > 0) value = input.Item2;
+                if (comparer.Compare(input.Item3, value) > 0) value = input.Item3;
+                if (comparer.Compare(input.Item4, value) > 0) value = input.Item4;
+                if (comparer.Compare(input.Item5, value) > 0) value = input.Item5;
+                if (comparer.Compare(input.Item6, value) > 0) value = input.Item6;
+                return value;
+            });
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<Tuple<TSource, TSource, TSource, TSource, TSource, TSource, TSource>> source)
+        {
+            var comparer = Comparer<TSource>.Default;
+            return source.Select(input =>
+            {
+                var value = input.Item1;
+                if (comparer.Compare(input.Item2, value) > 0) value = input.Item2;
+                if (comparer.Compare(input.Item3, value) > 0) value = input.Item3;
+                if (comparer.Compare(input.Item4, value) > 0) value = input.Item4;
+                if (comparer.Compare(input.Item5, value) > 0) value = input.Item5;
+                if (comparer.Compare(input.Item6, value) > 0) value = input.Item6;
+                if (comparer.Compare(input.Item7, value) > 0) value = input.Item7;
+                return value;
+            });
+        }
+
+        public IObservable<double> Process(IObservable<IEnumerable<double>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<float> Process(IObservable<IEnumerable<float>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<decimal> Process(IObservable<IEnumerable<decimal>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<ulong> Process(IObservable<IEnumerable<ulong>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<long> Process(IObservable<IEnumerable<long>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<uint> Process(IObservable<IEnumerable<uint>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<int> Process(IObservable<IEnumerable<int>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<ushort> Process(IObservable<IEnumerable<ushort>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<short> Process(IObservable<IEnumerable<short>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<sbyte> Process(IObservable<IEnumerable<sbyte>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<byte> Process(IObservable<IEnumerable<byte>> source)
+        {
+            return source.Select(Enumerable.Max);
+        }
+
+        public IObservable<TSource> Process<TSource>(IObservable<IEnumerable<TSource>> source)
+        {
+            return source.Select(Enumerable.Max);
         }
     }
 }
