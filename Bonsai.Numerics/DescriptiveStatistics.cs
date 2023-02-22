205ce3bbf8c3be95bbaec83bf37913b724da9a54
glopesdev <glopesdev@users.noreply.github.com>
2023-02-22 23:52:03 +0000

    Allow computing basic statistics over data streams

---
+++ Bonsai.Numerics/DescriptiveStatistics.cs
@@ -0,0 +1,30 @@
+﻿using MathNet.Numerics.Statistics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Numerics
+{
+    [Combinator]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    [Description("Computes basic statistics for each data buffer in the input sequence.")]
+    public class DescriptiveStatistics
+    {
+        public IObservable<RunningStatistics> Process(IObservable<double> source)
+        {
+            return source.Select(data =>
+            {
+                var statistics = new RunningStatistics();
+                statistics.Push(data);
+                return statistics;
+            });
+        }
+
+        public IObservable<RunningStatistics> Process(IObservable<IList<double>> source)
+        {
+            return source.Select(data => new RunningStatistics(data));
+        }
+    }
+}
